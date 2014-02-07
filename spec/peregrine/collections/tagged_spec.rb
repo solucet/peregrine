d5475d903670ca815f7a389b8d4656423d69003e
Solucet <solucet@icloud.com>
2014-02-07 03:24:35 -0500

    Fixed for Rubinius.

--- spec/peregrine/collections/tagged_spec.rb
+++ spec/peregrine/collections/tagged_spec.rb
@@ -1,25 +1,26 @@
 require 'spec_helper'
 
 describe Peregrine::Collections::Tagged do
-  let(:collection) { [:test, :symbol, :untagged].extend(subject) }
-  before(:each) do
-    Symbol.send(:include, Peregrine::Features::Taggable)
-    :test.add_tags('test', 'symbol')
-    :symbol.add_tags('symbol')
+  let(:entity) { Peregrine::Entity }
+  let(:collection) do
+    array = [ entity.new, entity.new, :untagged ]
+    array[0].add_tags('test', 'entity')
+    array[1].add_tags('entity')
+    array.extend(subject)
   end
   
   describe '#tagged' do
     it 'returns an array of items with all of the given tags' do
-      expect(collection.tagged('test', 'symbol')).to eql [collection.first]
+      expect(collection.tagged('test', 'entity')).to eql [collection.first]
     end
     
     context 'when given a block' do
       it 'yields each matching item' do
         expectation = []
-        collection.tagged('test', 'symbol') do |item|
+        collection.tagged('test', 'entity') do |item|
           expectation << item
         end
-        expect(expectation).to eql collection.tagged('test', 'symbol')
+        expect(expectation).to eql collection.tagged('test', 'entity')
       end
     end
     
@@ -31,16 +32,16 @@ describe Peregrine::Collections::Tagged do
   
   describe '#any_tagged' do
     it 'returns an array of items with any of the given tags' do
-      expect(collection.any_tagged('test', 'symbol')).to eql collection[0..1]
+      expect(collection.any_tagged('test', 'entity')).to eql collection[0..1]
     end
     
     context 'when given a block' do
       it 'yields each matching item' do
         expectation = []
-        collection.any_tagged('test', 'symbol') do |item|
+        collection.any_tagged('test', 'entity') do |item|
           expectation << item
         end
-        expect(expectation).to eql collection.any_tagged('test', 'symbol')
+        expect(expectation).to eql collection.any_tagged('test', 'entity')
       end
     end  
     
